5e0db78d9e6a011b7dbce1030f89affc6bdc0fd1
Ivan <ivanchekan07@gmail.com>
2019-10-30 23:38:52 +0500

    Сверстал, slider, и блок content

---
+++ src/js/slider-site.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 1500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		// dots: true,
+	});
+});
